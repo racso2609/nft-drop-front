c4a3c59cbeedf890803036e00987b51ccbf822b6
Oscar Lugo <oscaremiliolugo@gmail.com>
2022-02-27 16:07:43 +0000

    initial setup connect metamask

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import DropFactoryContract from "./Contracts/NftDrop.json";
+import "./App.css";
 
 function App() {
+  const [currentAccount, setCurrentAccount] = useState(null);
+
+  const checkWalletIsConnected = () => {
+    const { ethereum } = window;
+    if (!ethereum) return alert("install metamask");
+    connectWalletHandler();
+  };
+
+  const connectWalletHandler = async() => {
+    try {
+      const { ethereum } = window;
+      if (!ethereum) return alert("make sure hace metamask installed");
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      if (!accounts.length) return alert("autorized account nor found");
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      return alert(error.message);
+    }
+  };
+
+  const mintNftHandler = () => {};
+
+  const connectWalletButton = () => {
+    return (
+      <button
+        onClick={connectWalletHandler}
+        className="cta-button connect-wallet-button"
+      >
+        Connect Wallet
+      </button>
+    );
+  };
+
+  const mintNftButton = () => {
+    return (
+      <button onClick={mintNftHandler} className="cta-button mint-nft-button">
+        Mint NFT
+      </button>
+    );
+  };
+
+  useEffect(() => {
+    checkWalletIsConnected();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-app">
+      <h1>Scrappy Squirrels Tutorial</h1>
+      {currentAccount ? mintNftButton() : connectWalletButton()}
     </div>
   );
 }
